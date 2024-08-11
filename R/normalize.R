11cbf4ff474db86767adf47060ad97a8b1f0558a
danielmaangi <danielmaangi@gmail.com>
2024-08-11 17:35:21 +0300

    mantain vector length in normalize

--- R/normalize.R
+++ R/normalize.R
@@ -4,13 +4,18 @@
 #'
 #' @param x A numeric vector to be normalized.
 #' @param na.rm A logical value indicating whether to remove NA values before normalization. Default is FALSE.
-#' @return A numeric vector normalized to the range [0, 1].
+#' @return A numeric vector normalized to the range [0, 1], maintaining the original length.
 #' @examples
 #' normalize(c(1, 2, 3, 4, 5))
 #' normalize(c(1, 2, NA, 4, 5), na.rm = TRUE)
 normalize <- function(x, na.rm = FALSE) {
   if (na.rm) {
-    x <- x[!is.na(x)]
+    x_non_na <- x[!is.na(x)]
+    normalized <- (x_non_na - min(x_non_na)) / (max(x_non_na) - min(x_non_na))
+    result <- rep(NA, length(x))
+    result[!is.na(x)] <- normalized
+    return(result)
+  } else {
+    return ((x - min(x, na.rm = na.rm)) / (max(x, na.rm = na.rm) - min(x, na.rm = na.rm)))
   }
-  return ((x - min(x, na.rm = na.rm)) / (max(x, na.rm = na.rm) - min(x, na.rm = na.rm)))
 }
